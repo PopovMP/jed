a597e535f0c8c7b32314b07696138f72f3656b7b
Miroslav Popov <info@forexsb.com>
2023-10-18 13:46:58 +0000

    Extracted arg module

--- index.js
+++ index.js
@@ -3,67 +3,41 @@
 
 const proc = require("node:process");
 const fs   = require("node:fs");
-const {color, bright} = require("./lib/color.js");
+const {brightRed} = require("./lib/color.js");
+const {parseArgs} = require("./lib/arg.js");
 
-parseArgs();
+const state = {
+    prompt  : "",
+    filename: "",
+    mode    : "cmd",
+    curLine : 0,
+    lines   : [],
+};
 
-function parseArgs() {
-    proc.env.prompt = "";
+const argsDto = parseArgs(proc.argv);
+state.prompt   = argsDto.prompt;
+state.filename = argsDto.filename;
 
-    if (proc.argv.length === 2) {
-        // node jed
-        proc.env.filename = "";
-        main();
-        return;
-    }
-
-    if (proc.argv.length === 3) {
-        // node jed hello
-        proc.env.filename = proc.argv[2];
-        readFile();
-        return;
-    }
-
-    if (proc.argv.length === 5) {
-        if (proc.argv[2] === "-p") {
-            // node jed -p 'jed> ' hello
-            proc.env.prompt   = proc.argv[3];
-            proc.env.filename = proc.argv[4];
-            readFile();
-        } else {
-            // node jed hello -p 'jed> '
-            proc.env.prompt   = proc.argv[4];
-            proc.env.filename = proc.argv[2];
-            readFile();
-        }
-        return;
-    }
-
-    console.error(brightRed("Wrong arguments. Given: " +
-                            proc.argv.slice(2).join(" ")));
-    proc.exit(1);
+if (state.filename !== "") {
+    readFile(state.filename);
 }
 
-function readFile() {
-    fs.readFile(proc.env.filename, {encoding: "utf8"}, fs_readfile_ready);
+function readFile(filename) {
+    fs.readFile(filename, {encoding: "utf8"}, fs_readfile_ready);
 }
 
 function fs_readfile_ready(err, content) {
     if (err) {
         console.error(brightRed(err));
-        proc.exit(1);
+    } else {
+        console.log(content);
     }
 
-    console.log( content );
     main();
 }
 
 
 function main() {
-    console.log(proc.env.prompt + "Ready!");
-}
-
-function brightRed(msg) {
-    return `${color.Red}${color.Bright}${msg}${color.Reset}`;
+    console.log(state.prompt + "Ready!");
 }
 
