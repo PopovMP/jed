46f4821cf3fdc22aea8be9159acf6678dc50050b
Miroslav Popov <info@forexsb.com>
2023-10-22 16:47:50 +0000

    Extracted an "io" module.

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 "use strict";
 
 const proc     = require("node:process");
-const fs       = require("node:fs");
 const readline = require("node:readline");
 const os       = require("node:os");
 
-const {state}     = require("./lib/state.js");
-const {brightRed, color} = require("./lib/color.js");
-const {parseArgs} = require("./lib/arg.js");
+const {state} = require("./lib/state.js");
+const {color} = require("./lib/color.js");
+const {parseArgs}           = require("./lib/arg.js");
+const {readFile, writeFile} = require("./lib/io.js");
 
 const rl = readline.createInterface({input: proc.stdin, output: proc.stdout});
 rl.on("line", rl_on_line);
@@ -16,22 +16,11 @@ rl.on("line", rl_on_line);
 parseArgs(proc.argv);
 
 if (state.filename !== "") {
-    readFile(state.filename);
+    readFile(state.filename, (lines) => {
+        state.lines.push(...lines);
+        main();
+    });
 } else {
-    main();
-}
-
-function readFile(filename) {
-    fs.readFile(filename, {encoding: "utf8"}, fs_readfile_ready);
-}
-
-function fs_readfile_ready(err, content) {
-    if (err) {
-        console.error(brightRed(err));
-    } else {
-        state.lines = content.trimEnd().split(/\r?\n/);
-    }
-
     main();
 }
 
@@ -58,7 +47,9 @@ function rl_on_line(rowInput) {
 
     // Write file
     if (input === "w") {
-        writeFile();
+        const content = state.lines.join(os.EOL);
+        writeFile(state.filename, content, main);
+        return;
     }
 
     main();
@@ -73,19 +64,3 @@ function quit() {
     console.log("Goodbye :)");
     proc.exit(0);
 }
-
-function writeFile() {
-    const content = state.lines.join(os.EOL);
-    fs.writeFile(state.filename, content, {encoding: "utf8"}, fs_writeFile_ready);
-}
-
-function fs_writeFile_ready(err) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("File saved");
-    }
-
-    main();
-}
-
