ea0d7963aa003411a9aa1ae5442c154b2b1f6ac8
Miroslav Popov <info@forexsb.com>
2023-10-22 19:18:08 +0000

    Extracted renderer.

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const proc     = require("node:process");
 const readline = require("node:readline");
 const os       = require("node:os");
 
-const {state} = require("./lib/state.js");
-const {brightRed, color} = require("./lib/color.js");
-const {parseArgs}           = require("./lib/arg.js");
-const {parseInput}          = require("./lib/parser.js");
+const {state}       = require("./lib/state.js");
+const {brightRed}   = require("./lib/color.js");
+const {parseArgs}   = require("./lib/arg.js");
+const {parseInput}  = require("./lib/parser.js");
+const {render}      = require("./lib/renderer.js");
 const {readFile, writeFile} = require("./lib/io.js");
 
 const rl = readline.createInterface({input: proc.stdin, output: proc.stdout});
@@ -26,16 +27,11 @@ if (state.filename !== "") {
 }
 
 function main() {
-    render();
-
-    const filename = `${color.Blue}${state.filename}${color.Reset}`;
-    const prompt   = `${filename} ${state.lines.length} ${state.curLine+1} > `;
-    rl.setPrompt(prompt);
-    rl.prompt(false);
+    render(rl);
 }
 
 function rl_on_line(rowInput) {
-    const input = rowInput.trimEnd();
+    const input    = rowInput.trimEnd();
     const inputDto = parseInput(input, state.curLine, state.lines.length);
 
     if (inputDto.command === "q") {
@@ -70,18 +66,19 @@ function rl_on_line(rowInput) {
     }
 
     if (inputDto.command === "change-current-line") {
-        state.curLine = inputDto.range[0];
+        const newLine = inputDto.range[0];
+
+        if (newLine < 0 || newLine > state.lines.length - 1) {
+            console.error(brightRed(`Wrong current line input: ${newLine+1}`));
+        } else {
+            state.curLine = newLine;
+        }
+
         main();
         return;
     }
 
-
     main();
-}
-
-function render() {
-    console.log(state.lines.map((line, i) =>
-        `${color.Yellow}${i+1}${color.Reset}\t${line}`).join(os.EOL));
 }
 
 function quit() {
